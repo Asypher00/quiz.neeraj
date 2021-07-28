1bd6556b1b4d2570055e732cd6eede2c84dbb72a
Asypher <>
2021-07-28 09:01:06 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+var readlineSync = require('readline-sync');
+var userName = readlineSync.question('What is your name? ');
+console.log("Welcome To the quiz " + userName);
+var score = 0;
+
+var high_scores = {
+  name : "Abhay",
+  high_score : 3 ,
+}
+
+function questions(question , answer)
+{
+  var my_Answer = readlineSync.question(question);
+  if (my_Answer === answer)
+  {console.log("You are correct ");
+  score = score + 1;
+  console.log("Current Score = " + score);}
+  else
+  {console.log("You are incorrect ");
+  score=score-1;
+  console.log("Current Score = " + score);
+  console.log("--------------------");
+  }
+
+}
+
+var question_one = {
+  question : "Where Do I live?",
+  answer : "Kolkata",
+
+}
+
+var question_two = {
+  question : "What is my favourite sport?",
+  answer : "Cricket",
+
+}
+ var question_three = {
+   question : "In which college do I study?",
+   answer : "IIEST Shibpur",
+ }
+
+var question_four = {
+  question : "Who is my favourite player?",
+  answer : "Virat Kohli",
+};
+
+var question_array = [question_one, question_two, question_three, question_four];
+for(var i = 0 ; i < question_array.length ; i++)
+{ var current_question = question_array[i];
+questions(current_question.question, current_question.answer);}
+
+if(score >= high_scores.high_score)
+{console.log("You are the new high scorer");}
+else{console.log("high score is " + high_scores.high_score);}
+
+
+  
+
+console.log("Your total score is " + score);
